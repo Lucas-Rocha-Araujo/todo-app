8ffc4ef9d1f72335fe1c7060ba4ee6391ad10b5e
LUCAS ROCHA DE ARAUJO <95543607+Lucas-Rocha-Araujo@users.noreply.github.com>
2023-04-25 11:11:47 -0300

    configurandoCors

--- src/config/database.js
+++ src/config/database.js
@@ -2,3 +2,7 @@ const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
 module.exports = mongoose.connect('mongodb://localhost/todo')
+
+// const mongoose = require('mongoose')
+// mongoose.Promise = global.Promise
+// module.exports = mongoose.connect('mongodb://127.0.0.1/todo')
